67e3e6968fb56e5901f5b727e349be9b5372e3b5
GraceKoffi <grace01koffi@gmail.com>
2025-05-20 14:18:14 +0200

    20/05 - update WebTests

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -4,15 +4,18 @@ import com.example.demo.data.Voiture;
 import com.example.demo.service.Echantillon;
 import com.example.demo.service.StatistiqueImpl;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -26,5 +29,17 @@ class WebTests {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Test
+    public void getStatistiques() throws Exception {
+        doNothing().when(statistiqueImpl).ajouter(new Voiture("Maza", 405000));
+        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(1, 405000));
+        mockMvc.perform(get("/statistique"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombreDeVoitures").value("1"))
+                .andExpect(jsonPath("$.prixMoyen").value("405000"))
+                .andReturn();
+    }
 
 }
